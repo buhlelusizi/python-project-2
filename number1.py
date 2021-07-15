e217bbf8e008f5a131d5c4ab5d6576697b8f9675
Buhle Lusizi <70804695+buhlelusizi@users.noreply.github.com>
2021-07-15 14:13:41 +0200

    Add files via upload

---
+++ number1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+a = [1,2,3,4]
+b = [-1,-2,-3,-4]
+
+
+# In[4]:
+
+
+import matplotlib
+
+
+# In[5]:
+
+
+import matplotlib.pyplot as plt
+
+
+# In[8]:
+
+
+plt.plot(a,b)
+
